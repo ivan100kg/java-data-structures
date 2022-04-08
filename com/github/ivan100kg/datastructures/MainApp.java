b034f8123baf019164da96ef22fed78a3c9f1a60
Ivan B <ivar110kg@gmail.com>
2022-04-08 16:38:20 +0400

    add MainApp.java

---
+++ com/github/ivan100kg/datastructures/MainApp.java
@@ -0,0 +1,40 @@
+package com.github.ivan100kg.datastructures;
+
+import java.util.*;
+
+public class MainApp {
+    public static void main(String[] args) {
+        final int SIZE = 10000000; // 10b
+
+        // primitive array
+        int[] pArray = new int[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            pArray[i] = (int) (Math.random() * SIZE);
+        }
+        long start = System.currentTimeMillis();
+
+
+        // ArrayList
+        List<Integer> aList = new ArrayList<>();
+        for (int i : pArray) aList.add(i);
+        testGet(aList);
+
+        // LinkedList
+        List<Integer> lList = new LinkedList<>(aList);
+        testGet(lList);
+
+
+        // Vector
+        List<Integer> vList = new Vector<>(aList);
+        testGet(vList);
+
+
+    }
+
+    static void testGet(List<Integer> list) {
+        final int INDEX = 4500000;
+        long start = System.currentTimeMillis();
+        int result = list.get(INDEX + (int) ((Math.random() * (100 - 1)) + 1));
+        System.out.println(list.getClass().getSimpleName() + " test get(index) time: " + (System.currentTimeMillis() - start));
+    }
+}
